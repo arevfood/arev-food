0c787d7b3fb1759f9c7ee274facf3748325f2330
Yosa Rama <yosamelody07@gmail.com>
2025-06-19 11:13:39 +0800

    feat: :sparkles: add default color and font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,20 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary_color: "#FF6223",
+        secondary_color: "#F9B83F",
+        accent_color_1: "#239F00",
+        accent_color_2: "#FFF9F1",
+        white_color: "#FFFFFF",
+        black_color: "#212121",
+      },
+      fontFamily: {
+        font_heading: ["Mona Sans"],
+        font_paragraph: ["Mona Sans"],
+      },
+    },
   },
   plugins: [],
 };

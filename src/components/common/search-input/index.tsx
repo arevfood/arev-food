2548e2380cbb20654f8886070d9a01e925895b14
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2025-09-03 09:02:53 +0800

    CU-86euq6jdw - [Feat]: Reset Search when do Filter

--- src/components/common/search-input/index.tsx
+++ src/components/common/search-input/index.tsx
@@ -74,6 +74,7 @@ const SearchInput: React.FC<propTypes> = ({
     setSelectedFilter({ health_conditions: [] });
     setFilterValue({});
     setOpenFilter(false);
+    setSearchInput('');
     onReset?.();
   };
 
@@ -139,6 +140,7 @@ const SearchInput: React.FC<propTypes> = ({
                 onClick={() => {
                   setOpenFilter(false);
                   handleSearchQuery();
+                  setSearchInput('');
                   onFilter?.();
                 }}
               >

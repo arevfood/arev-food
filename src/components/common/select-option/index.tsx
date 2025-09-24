ba7fa4cd62aad10e97435d65e45a9bb2bd8140d9
Muhammad Ivan Verdyansyah <116993861+ivanverdyansyah11@users.noreply.github.com>
2025-09-24 22:47:41 +0800

    CU-86eupnpd3 - [Fix]: Fix Select Option UI (#72)

--- src/components/common/select-option/index.tsx
+++ src/components/common/select-option/index.tsx
@@ -38,6 +38,7 @@ const CustomSelect: React.FC<Props> = ({
                         onIonChange={(e) => onChange(e.detail.value)}
                         interface="popover"
                         disabled={disabled}
+                        interfaceOptions={{ cssClass: "custom-select-popover" }}
                         className="w-full bg-white px-4 py-1 rounded-[100px] !text-black !font-semibold font-heading flex border-[3px] !border-bg_color_1"
                     >
                         {options.map((opt) => (
